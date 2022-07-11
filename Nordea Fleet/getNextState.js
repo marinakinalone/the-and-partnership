eb201d79b3064690972399f7be95ed8f8cd40c05
Marina Kinalone Simonnet <kinalone.dev@gmail.com>
2022-07-11 16:11:09 +0200

    clean code base and add alt text

--- Nordea Fleet/getNextState.js
+++ Nordea Fleet/getNextState.js
@@ -1,32 +1,41 @@
 const cars = [
     {
       image: "bil1.jpg",
-      footer: "footer1.png",
+      model: "Citroën ë-Berlingo fra kr. 3.199,-",
       bubble: "bubble1.png",
+      footer: "footer1.png",
+      footerInfo: "Inklusive frakt, lev. og reg.omk Oslo, Månedspris fra 3 199 kr. Etableringsgebyr: 1 990 kr. Totalpris: 265 931 kr vil variere med rentenivået. Priseksempel 5 års leasing: Startleie 72 001 kr / 60 mnd. / 100 000 km"
     },
     {
       image: "bil2.jpg",
-      footer: "footer2.png",
+      model: "Peugeot e-Expert fra kr. 3.499,-",
       bubble: "bubble2.png",
+      footer: "footer2.png",
+      footerInfo: "Inklusive frakt, lev. og reg.omk Oslo, Månedspris fra 3 499 kr. Etableringsgebyr: 1 990 kr. Totalpris: 308 669 kr vil variere med rentenivået. Priseksempel 5 års leasing: Startleie 96 739 kr / 60 mnd. / 100 000 km"
     },
     {
       image: "bil3.jpg",
-      footer: "footer3.png",
+      model: "Peugeot e-parter fra kr. 3.199,-",
       bubble: "bubble3.png",
+      footer: "footer3.png",
+      footerInfo: "Inklusive frakt, lev. og reg.omk Oslo, Månedspris fra 3 199 kr. Etableringsgebyr: 1 990 kr. Totalpris: 265 951 kr vil variere med rentenivået. Priseksempel 5 års leasing: Startleie 72 021 kr / 60 mnd. / 100 000 km"
     },
     {
       image: "bil4.jpg",
-      footer: "footer4.png",
+      model: "Opel Vivaro-e fra kr. 3.499,-",
       bubble: "bubble4.png",
+      footer: "footer4.png",
+      footerInfo: "Inklusive frakt, lev. og reg.omk Oslo, Månedspris fra 3 499 kr. Etableringsgebyr: 1 990 kr. Totalpris: 307 925 kr vil variere med rentenivået. Priseksempel 5 års leasing: Startleie 95 995 kr / 60 mnd. / 100 000 km"
     },
   ];
 
   const displayTime = 5000;
 
-  const setImgSrc = (fileName, id) => {
+  const setImgSrc = (fileName, id, altText = "") => {
     const element = document.getElementById(id);
     const url = `./` + fileName;
     element.src = url;
+    element.alt = altText
   };
 
   const toggleClass = (element, className) => {
@@ -39,9 +48,9 @@ const cars = [
     toggleClass(element, `bubble-${index}`)
     toggleClass(element, "tinRightIn")
     toggleClass(element, `bubble-${i + 1}`)
-    setImgSrc(car.image, "car");
-    setImgSrc(car.bubble, "bubble");
-    setImgSrc(car.footer, "footer")
+    setImgSrc(car.image, "car", car.model);
+    setImgSrc(car.bubble, "bubble", car.price);
+    setImgSrc(car.footer, "footer", car.footerInfo)
     setTimeout(() => {
         toggleClass(element, "tinRightIn")
         console.log(element.className)
