b74660a57215743485626a037661aa2ec1fc4d76
Marina Kinalone Simonnet <kinalone.dev@gmail.com>
2022-07-12 16:24:32 +0200

    add template for second banenr

--- presentation/src/components/LexusLine.js
+++ presentation/src/components/LexusLine.js
@@ -1,8 +1,43 @@
 import React from 'react'
+import Header from './Header';
 
 const LexusLine = () => {
+  const headerContent = "Lexus Line"
   return (
-    <div>LexusLine</div>
+    <>
+    <Header content={headerContent} />
+    <main className="presentation">
+    <section className="presentation__banner banner-lexus">
+    <div className="banner" id="lexus-line">
+          <iframe title="lexus-line" className="iframe__lexus-line" width="1000" height="400" src="/html/Lexus Line/index.html"></iframe>
+        </div>
+    </section>
+    <section className="presentation-details">
+      <h2>about the project</h2>
+      <p>To code a working banner based on a sketch. 1 format, 4 states.</p>
+      <details>
+        <summary>Code the banner's main template</summary>
+        <ul>
+          <li>export all the elements from adobe XD</li>
+          <li>use "developer's view" from adobe XD</li>
+          <li>implement a clicktag with Studio</li>
+        </ul>
+      </details>
+      <details>
+        <summary>Implement the logic to move from one state to another (using JavaScript)</summary>
+        <ul>
+          <li>flexibility and modularity</li>
+          <li>accessibility</li>
+        </ul>
+      </details>
+      <details>
+      
+        <summary>Add a "cool animation"</summary>
+        <a href="https://www.minimamente.com/project/magic/">Magic Library for CSS</a>
+      </details>
+    </section>
+    </main>
+    </>
   )
 }
 
